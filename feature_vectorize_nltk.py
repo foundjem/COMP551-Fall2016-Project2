d2a5ed8aa46259948c8af631301ce9d8f64cff95
cdglasz <cdglasz@ymail.com>
2016-10-05 21:19:10 -0400

    TF-IDF

--- feature_vectorize_nltk.py
+++ feature_vectorize_nltk.py
@@ -2,8 +2,9 @@ import sys, csv
 import numpy as np
 from scipy import sparse
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 
-
+tf_idf = False
 
 def read_data(file):
 	with open(file, 'rt') as f:
@@ -13,17 +14,6 @@ def read_data(file):
 	header = {h:i for i,h in enumerate(header)}
 	return header, data
 
-def write_data(file, header, data):
-	with open(file,'wb') as f:
-		writer = csv.writer(f, delimiter=',')
-		writer.writerow(header)
-		for row in data:
-			writer.writerow(row)
-
-
-
-
-
 # Main driver code
 if __name__ == '__main__':
 
@@ -32,19 +22,25 @@ if __name__ == '__main__':
 	############################################
 	print "Reading files........................",
 	sys.stdout.flush()
-	h1, l1 = read_data('datasets/train_in.csv')
-	h2, l2 = read_data('datasets/train_out.csv')
-	h3, l3 = read_data('datasets/test_in.csv')
-	ids_all, doc_all = zip(*l1)
-	ids_all, lab_all = zip(*l2)
-	ids_tst, doc_tst = zip(*l3)
-
-	# Split into training and validation sets
-	v_split = int(0.8 * len(ids_all))
-	ids_trn, ids_val = ids_all[:v_split], ids_all[v_split:]
-	doc_trn, doc_val = doc_all[:v_split], doc_all[v_split:]
-	lab_trn, lab_val = lab_all[:v_split], lab_all[v_split:]
+	dat_all = np.array(read_data('datasets/train_in.csv')[1])
+	dat_tst = np.array(read_data('datasets/test_in.csv')[1])
+	lab_all = np.array(read_data('datasets/train_out.csv')[1])
 	
+	# Split into training and validation sets
+	v_split = int(0.8 * dat_all.shape[0])
+	ids_trn = dat_all[:v_split,0].astype(int)
+	ids_val = dat_all[v_split:,0].astype(int)
+	ids_all = dat_all[:,0].astype(int)
+	ids_tst = dat_tst[:,0].astype(int)
+	
+	doc_trn = dat_all[:v_split,1]
+	doc_val = dat_all[v_split:,1]
+	doc_all = dat_all[:,1]
+	doc_tst = dat_tst[:,1]
+	
+	lab_trn = lab_all[:v_split,1]
+	lab_val = lab_all[v_split:,1]
+	lab_all = lab_all[:,1]
 	print "Done."
 
 	print "Building vectorizer..................",
@@ -54,33 +50,61 @@ if __name__ == '__main__':
 								 lowercase=True,
 								 max_features=1000,
 								 min_df=1)
+	ng_vectorizer = CountVectorizer(ngram_range=(2,3),
+									stop_words='english',
+									lowercase=True,
+									max_features=400,
+									min_df=1)
 	print "Done."
 
 	print "Extracting training features.........",
 	sys.stdout.flush()
 	X_trn = vectorizer.fit_transform(doc_trn)
+	X_trn = sparse.hstack((X_trn, ng_vectorizer.fit_transform(doc_trn)))
 	X_val = vectorizer.transform(doc_val)
+	X_val = sparse.hstack((X_val, ng_vectorizer.transform(doc_val)))
 	print "Done."
 	
 	print "Extracting testing features..........",
 	sys.stdout.flush()
 	X_all = vectorizer.fit_transform(doc_all)
+	X_all = sparse.hstack((X_all, ng_vectorizer.fit_transform(doc_all)))
 	X_tst = vectorizer.transform(doc_tst)
+	X_tst = sparse.hstack((X_tst, ng_vectorizer.transform(doc_tst)))
 	print "Done."
 	
+	if tf_idf:
+		print "Building TF-IDF transformer..........",
+		sys.stdout.flush()
+		transformer = TfidfTransformer()
+		print "Done."
+		
+		print "Applying TF-IDF transform............",
+		sys.stdout.flush()
+		X_trn = transformer.fit_transform(X_trn)
+		X_val = transformer.transform(X_val)
+		X_all = transformer.fit_transform(X_all)
+		X_tst = transformer.transform(X_tst)
+		print "Done."
+	
+#	print "Features:"
+#	for f in vectorizer.get_feature_names(): print "   "+f
+#	for f in ng_vectorizer.get_feature_names(): print "   "+f
+
+	# Preppend IDs
 	ids_trn = np.array(ids_trn).astype(int)[:,None]
 	ids_val = np.array(ids_val).astype(int)[:,None]
 	ids_all = np.array(ids_all).astype(int)[:,None]
 	ids_tst = np.array(ids_tst).astype(int)[:,None]
-	
-	Y_trn = np.array(lab_trn)[:,None]
-	Y_val = np.array(lab_val)[:,None]
-	Y_all = np.array(lab_all)[:,None]
 
 	X_trn =	sparse.hstack((ids_trn, X_trn))
 	X_val =	sparse.hstack((ids_val, X_val))
 	X_all =	sparse.hstack((ids_all, X_all))
 	X_tst =	sparse.hstack((ids_tst, X_tst))
+	
+	Y_trn = np.array(lab_trn)[:,None]
+	Y_val = np.array(lab_val)[:,None]
+	Y_all = np.array(lab_all)[:,None]
 
 	Y_trn =	np.hstack((ids_trn, Y_trn))
 	Y_val =	np.hstack((ids_val, Y_val))
@@ -88,10 +112,11 @@ if __name__ == '__main__':
 
 	print "Writing to file......................",
 	sys.stdout.flush()
-	np.savetxt('X_trn.csv', X_trn.toarray(), delimiter=',', fmt='%d')
-	np.savetxt('X_val.csv', X_val.toarray(), delimiter=',', fmt='%d')
-	np.savetxt('X_all.csv', X_all.toarray(), delimiter=',', fmt='%d')
-	np.savetxt('X_tst.csv', X_tst.toarray(), delimiter=',', fmt='%d')
+	fmt = '%f' if tf_idf else '%d'
+	np.savetxt('X_trn.csv', X_trn.toarray(), delimiter=',', fmt=fmt)
+	np.savetxt('X_val.csv', X_val.toarray(), delimiter=',', fmt=fmt)
+	np.savetxt('X_all.csv', X_all.toarray(), delimiter=',', fmt=fmt)
+	np.savetxt('X_tst.csv', X_tst.toarray(), delimiter=',', fmt=fmt)
 	
 	np.savetxt('Y_trn.csv', Y_trn, delimiter=',', fmt='%s')
 	np.savetxt('Y_val.csv', Y_val, delimiter=',', fmt='%s')
