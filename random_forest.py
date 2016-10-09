4b50f5073a08f4c7cd73d3d3c7ea8d99194cf0c8
hptruong93 <hptruong93@gmail.com>
2016-10-09 16:40:00 -0400

    Allow random forest to predict multi-class. Add GridSearch support.

--- random_forest.py
+++ random_forest.py
@@ -1,7 +1,10 @@
+import sys
 import math
 import random
+import collections
 
 import numpy as np
+from sklearn.grid_search import GridSearchCV
 
 POSITIVE_VALUE = 1
 NEGATIVE_VALUE = 0
@@ -9,13 +12,19 @@ MID_VALUE = 0.5
 
 log2 = lambda x : math.log(x, 2)
 
-def entropy(p, n):
-    if p == 0 or n == 0:
+def entropy(counts, total):
+    if len(counts) == 0 or total == 0:
         return 0
 
-    p_true = float(p) / (p + n)
-    p_false = 1 - p_true
-    return - p_true * log2(p_true) - p_false * log2(p_false)
+    assert sum(counts) == total
+
+    output = 0
+    total = float(total)
+    for count in counts:
+        p = count / total
+        output += - p * log2(p)
+
+    return output
 
 class Decision(object):
     """A Decision"""
@@ -75,27 +84,19 @@ class TreeNode(object):
         self.false = TreeNode(false_X, false_y)
 
     def entropy_root(self):
-<<<<<<< HEAD
         """
             Calculating entropy of this node
         """
-=======
->>>>>>> 1508c7ed82458f68e19908e5cfdff95069634c04
         if self.size() == 0:
             return 0
 
-        positive = sum(1 for value in self.y if value == POSITIVE_VALUE)
-        negative = self.size() - positive
-
-        return entropy(positive, negative)
+        counter = collections.Counter(self.y)
+        return entropy(map(lambda x : x[1], counter.items()), self.size())
 
     def entropy_children(self):
-<<<<<<< HEAD
         """
             Calculating weighted entropy of the children nodes.
         """
-=======
->>>>>>> 1508c7ed82458f68e19908e5cfdff95069634c04
         assert self.true
         assert self.false
 
@@ -105,19 +106,28 @@ class TreeNode(object):
 
 class RandomForest(object):
     """Implementation of random forest model"""
-    def __init__(self, k, m, metadata):
+    def __init__(self, metadata = None, k = 2, m = 5):
         super(RandomForest, self).__init__()
         self.k = k
         self.m = m
 
-        # Metadata contains the information about each input feature: either 'boolean' or 'continuous'
-        assert m <= len(metadata)
+        # Metadata contains the information about each input feature: either 'b' for boolean or 'c' for continuous
         self.metadata = metadata
+
+    def get_params(self, deep=True):
+        return {'k' : self.k, 'm' : self.m}
+
+    def set_params(self, **parameters):
+        for parameter, value in parameters.items():
+            setattr(self, parameter, value)
+
+        assert self.m <= len(self.metadata)
+        return self
 
     def _build_decision(self, feature_index, data):
         assert feature_index < len(self.metadata)
         feature_type = self.metadata[feature_index]
-        if feature_type == 'boolean':
+        if feature_type == 'b':
             return Decision(feature_index, MID_VALUE)
         else:
             min_value = min(data[:,feature_index])
@@ -173,46 +183,82 @@ class RandomForest(object):
                     non_zero_leaf_nodes[tree_index].append(leaf_node.false)
 
     def predict_single(self, x):
-        results = []
+        results = collections.Counter()
         for root in self.root_nodes:
             node = root
             while True: # Go down the tree
                 if node.is_leaf():
-                    results.append(node.predict(x))
+                    results[node.predict(x)] += 1
                     break
                 elif node.decision.test(x):
                     node = node.true
                 else:
                     node = node.false
 
-        positive_count = sum(1 for _ in results)
-        return POSITIVE_VALUE if positive_count >= len(results) else NEGATIVE_VALUE # Major vote
+        return results.most_common()[0][0]
 
     def predict(self, X):
         return [self.predict_single(x) for x in X]
 
-    def evaluate(self, X, y):
+    def score(self, X, y):
         prediction = self.predict(X)
-        error_count = sum(1 for index, value in enumerate(prediction) if value == y[index])
+        accurate_count = sum(1 for index, value in enumerate(prediction) if value == y[index])
 
-        error = float(error_count) / len(y)
-        print "Error rate: {0}".format(error)
-        return error
+        accuracy = float(accurate_count) / len(y)
+        print "Accuracy: {0}".format(accuracy)
+        return accuracy
 
 
 if __name__ == "__main__":
-    rf = RandomForest(3, 2, ['boolean', 'continuous', 'boolean'])
+    # rf = RandomForest(1, 2, ['b', 'c', 'b'])
 
-    X = np.array([
-                    [NEGATIVE_VALUE, 7, NEGATIVE_VALUE],
-                    [POSITIVE_VALUE, 6, POSITIVE_VALUE],
-                    [NEGATIVE_VALUE, 4, NEGATIVE_VALUE],
-                    [POSITIVE_VALUE, 5, POSITIVE_VALUE],
-                    [POSITIVE_VALUE, 3, POSITIVE_VALUE],
-                    [NEGATIVE_VALUE, 9, NEGATIVE_VALUE],
-                    [NEGATIVE_VALUE, 12, NEGATIVE_VALUE],
-                    [POSITIVE_VALUE, 14, POSITIVE_VALUE]
-                    ])
-    y = [POSITIVE_VALUE, NEGATIVE_VALUE, POSITIVE_VALUE, POSITIVE_VALUE, POSITIVE_VALUE, NEGATIVE_VALUE, NEGATIVE_VALUE, POSITIVE_VALUE]
-    rf.fit(X, y)
-    rf.evaluate(X, y)
+    # X = np.array([
+    #                 [NEGATIVE_VALUE, 7, NEGATIVE_VALUE],
+    #                 [NEGATIVE_VALUE, 6, POSITIVE_VALUE],
+    #                 [POSITIVE_VALUE, 4, NEGATIVE_VALUE],
+    #                 [NEGATIVE_VALUE, 5, POSITIVE_VALUE],
+    #                 [POSITIVE_VALUE, 3, POSITIVE_VALUE],
+    #                 [NEGATIVE_VALUE, 9, NEGATIVE_VALUE],
+    #                 [NEGATIVE_VALUE, 12, NEGATIVE_VALUE],
+    #                 [POSITIVE_VALUE, 14, POSITIVE_VALUE]
+    #                 ])
+    # y = [3, NEGATIVE_VALUE, POSITIVE_VALUE, 4, POSITIVE_VALUE, NEGATIVE_VALUE, POSITIVE_VALUE, NEGATIVE_VALUE]
+    # rf.fit(X, y)
+    # rf.score(X, y)
+
+    ############################################
+    ################# Get data #################
+    ############################################
+    print "Reading files........................",
+    sys.stdout.flush()
+
+    X_trn = np.genfromtxt('extracted_features/X_trn.csv', delimiter=',', dtype=int)
+    X_val = np.genfromtxt('extracted_features/X_val.csv', delimiter=',', dtype=int)
+    # X_all = np.genfromtxt('extracted_features/X_all.csv', delimiter=',', dtype=int)
+    X_tst = np.genfromtxt('extracted_features/X_tst.csv', delimiter=',', dtype=int)
+
+    ids_trn, X_trn = X_trn[:,0][:,None], X_trn[:,1:]
+    ids_val, X_val = X_val[:,0][:,None], X_val[:,1:]
+    # ids_all, X_all = X_all[:,0][:,None], X_all[:,1:]
+    ids_tst, X_tst = X_tst[:,0][:,None], X_tst[:,1:]
+
+    features_count = X_trn.shape[1]
+
+    Y_trn = np.genfromtxt('extracted_features/Y_trn.csv', delimiter=',', dtype=str, usecols=[1])
+    Y_val = np.genfromtxt('extracted_features/Y_val.csv', delimiter=',', dtype=str, usecols=[1])
+    # Y_all = np.genfromtxt('extracted_features/Y_all.csv', delimiter=',', dtype=str, usecols=[1])
+
+    print "Done."
+
+    print "Training classifier..................",
+    sys.stdout.flush()
+    model = RandomForest(['c' for i in xrange(features_count)])
+    model = GridSearchCV(model, {'k':[2,10,20,30,50,100], 'm': [2,4,8,16], 'metadata' : [['c' for i in xrange(features_count)]]}, cv = 10)
+
+    model.fit(X_trn, Y_trn)
+    print "Done."
+
+    print "Testing on validation data...........",
+    sys.stdout.flush()
+    accuracy = model.score(X_val, Y_val)
+    print "Done."
